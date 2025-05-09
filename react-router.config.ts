95d8b6ce6294585f41cc54501b821f5d671c7782
Ayaan Zaidi <zaidi@uplause.io>
2025-05-09 16:35:28 +0530

    fix: exclude API only routes from pre-rendering

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "@react-router/dev/config";
+
+const doNotPrerender = ["/admin", "/auth/discord", "/auth/discord/callback"];
+
 export default {
 	ssr: true,
-	prerender: true,
+	prerender: async ({ getStaticPaths }) => {
+		const paths = await getStaticPaths();
+
+		// Filter out loader only routes
+		return paths.filter((path) => !doNotPrerender.includes(path));
+	},
 } satisfies Config;
