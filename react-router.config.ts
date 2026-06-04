cb576039b8e5e78f3b9b6cdd347ffa73f49c45db
Ayaan Zaidi <hi@obviy.us>
2026-06-05 00:08:07 +0530

    build(router): enable vite environment api

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,6 +5,7 @@ export default {
 		v8_middleware: true,
 		v8_splitRouteModules: true,
 		v8_trailingSlashAwareDataRequests: true,
+		v8_viteEnvironmentApi: true,
 	},
 	ssr: true,
 } satisfies Config;
